a868aca6c64791a46ffee9aa1ac83fb83df5b570
laluridho <lalukurnia22@gmail.com>
2024-11-29 10:59:54 +0800

    fix bugs host value

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const authRoutes = require("./routes/authRoutes");
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
-    host: "localhost",
+    host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
   });
 
   server.route(authRoutes);
